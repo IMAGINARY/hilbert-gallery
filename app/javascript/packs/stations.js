e8c09ec128c9a3f86a191765f2972800a857fbfb
Eric Londaits <eric.londaits@gmail.com>
2022-06-23 17:01:53 +0200

    Add a debug log message to the station.

--- app/javascript/packs/stations.js
+++ app/javascript/packs/stations.js
@@ -1,8 +1,17 @@
+function showError(text) {
+  document.getElementById('errorLog').innerHTML = text;
+}
+
 $('[data-station]').each((i, element) => {
   const stationID = $(element).attr('data-station');
   console.log('Station %s waiting', stationID);
-  window.connectMediaChannel(stationID, (data) => {
-    const viewer = document.getElementById('viewer');
-    viewer.execute(data.action, data.args);
-  });
+  try {
+    window.connectMediaChannel(stationID, (data) => {
+      const viewer = document.getElementById('viewer');
+      viewer.execute(data.action, data.args);
+      showError('');
+    });
+  } catch (err) {
+    showError(err.message);
+  }
 });
